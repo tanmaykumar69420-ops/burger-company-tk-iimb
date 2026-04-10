fcbbb192f6f6bf79950addadb6ac2bcd0433a9f0
tanmay kumar <tanmaykumar69420@gmail.com>
2026-04-10 15:38:26 +0530

    Add slideshow script for navigating slides
    Implement a simple slideshow functionality with next and previous controls.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+<script>
+let index = 0;
+const slides = document.querySelectorAll(".slide");
 
+function showSlide(i) {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[i].classList.add("active");
+}
+
+document.querySelector(".next").onclick = () => {
+    index = (index + 1) % slides.length;
+    showSlide(index);
+};
+
+document.querySelector(".prev").onclick = () => {
+    index = (index - 1 + slides.length) % slides.length;
+    showSlide(index);
+};
+</script>
